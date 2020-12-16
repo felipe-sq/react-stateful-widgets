5b18d0239f4f78610caf5b22e483a5d48cf2585d
Felipe Quintero <ffquint@gmail.com>
2020-12-16 16:40:51 -0500

    completed all steps in moods.js

--- src/components/Moods.js
+++ src/components/Moods.js
@@ -39,12 +39,15 @@ export default function Moods() {
   const [mood, setMood] = useState(initialMood);
   const makeHappy = () => {
     /* STEP 4 */
+    setMood(happyMood);
   };
   const makeSad = () => {
     /* STEP 5 */
+    setMood(sadMood);
   };
   const reset = () => {
     /* STEP 6 */
+    setMood(initialMood);
   };
 
   const style = {
@@ -56,7 +59,7 @@ export default function Moods() {
   return (
     <div className='widget-moods container'>
       <h2>Moods</h2>
-      <div id='mood' style={style}>Not sure how I feel</div> {/* STEP 3 */}
+      <div id='mood' style={style}>{mood}</div> {/* STEP 3 */}
       <div>
         <button id='makeHappy' onClick={makeHappy}>Make Happy</button>
         <button id='makeSad' onClick={makeSad}>Make Sad</button>
