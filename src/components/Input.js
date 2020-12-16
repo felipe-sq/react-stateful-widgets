464faf3482a67a658b23d5007c8a8cd28f2ef1c5
Felipe Quintero <ffquint@gmail.com>
2020-12-16 17:38:42 -0500

    completed all steps through step 4

--- src/components/Input.js
+++ src/components/Input.js
@@ -43,7 +43,7 @@ export default function Input() {
     // When the input changes, its whole value can be found inside the event object.
     // Log out the synthetic event object 'evt' and see for yourself.
     const { value } = evt.target;
-
+    setInputValue(value.toUpperCase());
     /* STEP 4 */
   };
   const reset = () => {
@@ -53,13 +53,13 @@ export default function Input() {
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: 'royalblue', /* STEP 2 */
+    color: (inputValue.length <= 10 ? 'royalblue' : 'crimson')/* STEP 2 */
   };
 
   return (
     <div className='widget-input container'>
       <h2>Input</h2>
-      <div id='output' style={style}></div> {/* STEP 3 */}
+      <div id='output' style={style}>{inputValue}</div> {/* STEP 3 */}
       <div>
         <input id='input' type='text' onChange={changeInput} /> {/* STEP 6 */}
         <button id='resetInput' onClick={reset}>Reset</button>
