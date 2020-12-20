c607320e59bedde4f7b4b41281a056cc94fb19a8
Felipe Quintero <ffquint@gmail.com>
2020-12-20 15:06:53 -0500

    working on code for Programmers.js; code is still not working for setting the featured id of the selected programmer. It appears to be something with the programmer Id not being passed through the getNameOfFeatured() function.

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -28,8 +28,8 @@ export default function Programmers() {
   // We'll have to use the state hook twice, as we need two slices of state.
   // The programmers list on the one hand, and the id of the featured programmer on the other.
   const [programmers, setProgrammers] = useState(listOfAwesome)
-  const [featuredId, setFeaturedId] = useState('')
-  // console.log(programmers);
+  const [featuredId, setFeaturedId] = useState(null)
+  // console.log(programmers[1].name);
 
   const getNameOfFeatured = (id) => {
     // Leave this for last!
@@ -37,7 +37,7 @@ export default function Programmers() {
     // It's going to utilize both slices of state to return the _name_ of the featured dev.
     // The beauty of closures is that we can "see" both slices of state from this region
     // of the program, without needing to inject the information through arguments.
-    (id === programmers.id ? setFeaturedId('featured') : setFeaturedId(''))
+    (id === featuredId ? setFeaturedId(programmers[id]) : setFeaturedId(null))
     return
   };
 
