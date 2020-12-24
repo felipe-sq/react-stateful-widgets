6fe1827142a99db798343d64ae2cc3b4dff2ad19
Felipe Quintero <ffquint@gmail.com>
2020-12-24 15:52:17 -0500

    updated code on spinner.js; removed brackets from the variable 'spinnerOn' on line 55 in order to get the show/hide function to work properly

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -52,7 +52,7 @@ export default function Spinner() {
     <div className='widget-spinner container'>
       <h2>Spinner</h2>
       {
-        {spinnerOn} && <div id='spinner' className='spinner'>--+--</div> /* STEP 2 */
+        spinnerOn && <div id='spinner' className='spinner'>--+--</div> /* STEP 2 */
       }
       <button id='toggleSpinner' onClick={toggleSpinner}>
         { spinnerOn ? 'Hide' : 'Show' } Spinner {/* STEP 3 */}
